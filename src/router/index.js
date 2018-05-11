a44c65ee891840a8eea68ce4b3fd83055b37df70
tonimota <toni.elton1993@gmail.com>
2018-05-11 09:50:31 -0300

    [FEAT] Berrs integration

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Callback',
+      name: 'Callback',
+      component: HelloWorld
     }
   ]
 })
